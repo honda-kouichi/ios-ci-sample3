e81f2232682b3ee19405432c3ff1233a43ec8d7c
honda kouichi <honda.kouichi@actindi.net>
2015-12-08 15:27:57 +0900

    プロジェクトを追加

---
+++ ios-ci-sample3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-ci-sample3
+//
+//  Created by kouichi honda on 2015/12/08.
+//  Copyright © 2015年 kouichi honda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
